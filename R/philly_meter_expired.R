a96032d8bb5c308011e07b76b7596ffcc12f1168
Sebastian <sebtorres7021@gmail.com>
2019-12-03 18:11:04 -0500

    update animations

--- R/philly_meter_expired.R
+++ R/philly_meter_expired.R
@@ -5,6 +5,7 @@ library(rayshader)
 library(sf)
 library(tidycensus)
 library(gganimate)
+library(lubridate)
 tickets <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-03/tickets.csv")
 philly <- get_acs(geography = "tract",county = "Philadelphia",state = "PA",variables = "B19013_001",
                   geometry = T)
@@ -56,7 +57,7 @@ philly_meter_counts %>%
   geom_rect(data= box,aes(xmin = xmin - .05,ymin = ymin-.01,xmax = xmax+ .01,ymax = ymax+.01),fill= "transparent",color = "black")+
   coord_sf(expand = T)+
   labs(title = "Total number of Meter Expiration Tickets in Philadelphia",
-       subtitle = "from 2013-2017")+
+       subtitle = "In 2017")+
   annotation_north_arrow(style = north_arrow_fancy_orienteering(),location = "tl",pad_x = unit(0.5,"cm"),
                          pad_y = unit(0.75,"cm"))+
   theme(plot.title = element_text(hjust = 0.5,family = "Times"),
@@ -67,3 +68,43 @@ philly_meter_counts %>%
         strip.background = element_rect(fill= "grey80"))
 
 ggsave("all_meter.png")
+
+
+# Day 2 of code -----------------------------------------------------------
+364 *12
+philly_tib <- philly %>% as_tibble()
+philly_meter_month <- philly_meter %>% 
+  mutate(months1 = fct_explicit_na(lubridate::month(issue_datetime,label = T,abbr = FALSE))) %>% 
+  count(months1,NAME,.drop = F) %>% 
+  complete(months1,NAME,fill = list(n = 0)) %>% 
+  st_as_sf() %>% 
+  mutate(months1 = fct_recode(months1, "January" ="(Missing)")) 
+
+
+
+test <- full_join(philly_meter_month,philly_tib,by = "NAME") 
+palettes_c_names
+test %>% 
+  as_tibble() %>% 
+  select(-geometry.x) %>%
+  st_as_sf() %>% 
+  ggplot() +
+  geom_sf(aes(fill = n),color = "white",size = 0.2) +
+  scale_fill_paletteer_c("gameofthrones", "lannister",labels = scales::comma) +
+  theme_void() +
+  geom_rect(data= box,aes(xmin = xmin - .05,ymin = ymin-.01,xmax = xmax+ .01,ymax = ymax+.01),fill= "transparent",color = "black")+
+  coord_sf(expand = T)+
+  labs(title = "Number of Meter Expiration Tickets in Philadelphia",
+       subtitle = "In {current_frame}",
+       fill = "Total")+
+  annotation_north_arrow(style = north_arrow_fancy_orienteering(),location = "tl",pad_x = unit(0.5,"cm"),
+                         pad_y = unit(0.75,"cm"))+
+  theme(plot.title = element_text(hjust = 0.5,family = "Times",face = "bold"),
+        plot.subtitle = element_text(hjust= 0.5,family = "Times"),
+        legend.position = c(.87,0.2),
+        plot.background = element_rect(fill = "grey80"),
+        panel.background = element_rect(fill = "grey80"),
+        strip.background = element_rect(fill= "grey80"),
+        legend.title = element_text())+
+  transition_manual(months1)
+anim_save("monthly_meter.gif")
